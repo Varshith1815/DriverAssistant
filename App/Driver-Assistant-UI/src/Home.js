56c689d24c97b32327e5c35dd12495b295387107
akshay kola <akola1@asu.edu>
2024-02-26 23:45:14 -0700

    Restructure code

--- App/Driver-Assistant-UI/src/Home.js
+++ App/Driver-Assistant-UI/src/Home.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View  style={styles.container}>
+      <Text>In Home</Text>
+    </View >
   );
 }
 
@@ -18,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default Home;
