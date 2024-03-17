1e2950102248fc0ebe8882220076c7a1f0862287
hvenka17 <hvenka17@asu.edu>
2024-03-17 13:39:45 -0700

    #72: integrated tomtom API

--- App/Driver-Assistant-UI/src/Speedometer.js
+++ App/Driver-Assistant-UI/src/Speedometer.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Switch } from 'react-native';
 import * as Location from 'expo-location';
 import { AntDesign } from '@expo/vector-icons'; // Ensure you have expo/vector-icons installed
+import axios from 'axios';
 
 const Speedometer = () => {
   const [speedKmh, setSpeedKmh] = useState(0);
   const [useMph, setUseMph] = useState(false); // State to toggle between km/h and mph
+  const [speedLimit, setSpeedLimit] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -25,6 +27,7 @@ const Speedometer = () => {
         (location) => {
           const currentSpeedKmh = location.coords.speed * 3.6;
           setSpeedKmh(currentSpeedKmh >= 0 ? currentSpeedKmh : 0);
+          getSpeedLimit();
         }
       );
     })();
@@ -34,10 +37,36 @@ const Speedometer = () => {
   const speedText = speed.toFixed(1) + (useMph ? ' mph' : ' km/h');
   const speedColor = speed < 30 ? '#30b455' : speed < 70 ? '#e3b23c' : '#d9534f';
 
+  const apiKey = 'c63IvblzdGjwYAe8HLcEbunVMGIi5LGr';
+  // const coordinates = [
+  //   '-73.9856644,40.7484405',
+  //   '-74.00029,40.76254'
+  // ]
+  
+  // NOTE: format = 'long,lat'
+  const coordinates = [
+    '-111.932285,33.422002', // University Dr
+    '-111.932285,33.422002'
+  ]
+  
+  const apiUrl = `https://api.tomtom.com/snap-to-roads/1/snap-to-roads?points=${coordinates.join(';')}&fields={route{type,geometry{type,coordinates},properties{id,speedRestrictions{maximumSpeed{value,unit}}}}}&key=${apiKey}`
+  const getSpeedLimit = () => {
+    axios.get(apiUrl)
+      .then(res => {
+        res.data.route.forEach(
+          (item) => {
+            console.log(item.properties.speedRestrictions.maximumSpeed.value);
+            setSpeedLimit(item.properties.speedRestrictions.maximumSpeed.value);
+          }
+        )
+      });
+  }
+
   return (
     <View style={[styles.container, { borderColor: speedColor }]}>
       <AntDesign name={speed < 30 ? 'caretright' : 'rocket1'} size={48} color={speedColor} />
       <Text style={[styles.speedText, { color: speedColor }]}>{errorMsg || speedText}</Text>
+      <Text style={[styles.speedText, { color: speedColor }]}>{speedLimit !== 0 ? `Limit: ${speedLimit}` : ''}</Text>
       <Switch
         trackColor={{ false: "#767577", true: "#81b0ff" }}
         thumbColor={useMph ? "#f5dd4b" : "#f4f3f4"}
